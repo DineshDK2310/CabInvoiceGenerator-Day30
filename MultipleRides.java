15dc0492452eda28289b98c154c2b04bdf9e5cce
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-02-16 16:00:39 -0800

    Adding:MultipleRided File Added Successfully

---
+++ MultipleRides.java
@@ -0,0 +1,13 @@
+package CabInvoiceProblem;
+
+public class MultipleRides {
+	public double distance;
+	public int time;
+
+	public MultipleRides(double distance,int time)
+	{
+		this.distance = distance;
+		this.time = time; 
+	}
+}
+
